de9af8fe71e191ac1fca24c96c582f9cd6b28ad7
ericj <ericjesus403@gmail.com>
2024-04-24 09:39:45 -0300

    Arquivo responsável pela criação do pod django em kubernetes

--- teste/settings.py
+++ teste/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1!58j#77=1$=c85)m3gae=te*f36sjo)o&9)qcv9kouj$_al@9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE = [
