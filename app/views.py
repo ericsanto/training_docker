0230726414addd76366d3a90160f9ff568e0d89b
ericj <ericjesus403@gmail.com>
2024-04-24 09:33:53 -0300

    Criando view para testar a sanidade do pod kubernetes

--- app/views.py
+++ app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse    
+from http import HTTPStatus
 
 def home(request):
     return render(request, 'home.html')
+
+
+def sanit_kubernetes(request):
+    return render(request, 'success.html')
